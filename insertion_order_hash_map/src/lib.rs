2346b129776475daa2ff6c9d59284bab5113d934
Eduardo R. D'Avila <erdavila@gmail.com>
2023-01-27 14:09:37 -0300

    Implement basic operations

---
+++ insertion_order_hash_map/src/lib.rs
@@ -0,0 +1,133 @@
+use std::collections::{hash_map::Entry, HashMap};
+use std::hash::Hash;
+use std::marker::PhantomData;
+use std::mem;
+use std::ptr::NonNull;
+
+#[cfg(test)]
+mod tests;
+
+pub struct InsertionOrderHashMap<K, V> {
+    nodes: HashMap<Box<K>, Box<Node<K, V>>>,
+    order: Option<InsertionOrder<K, V>>,
+}
+impl<K, V> InsertionOrderHashMap<K, V> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn get(&self, key: &K) -> Option<&V>
+    where
+        K: Hash + Eq,
+    {
+        self.nodes.get(key).map(|node| &node.value)
+    }
+
+    pub fn set(&mut self, key: K, value: V) -> Option<V>
+    where
+        K: Hash + Eq,
+    {
+        match self.nodes.entry(Box::new(key)) {
+            Entry::Occupied(mut occupied) => {
+                let previous_value = mem::replace(&mut occupied.get_mut().value, value);
+                Some(previous_value)
+            }
+            Entry::Vacant(vacant) => {
+                let node = Box::new(Node {
+                    key: NonNull::from(vacant.key().as_ref()),
+                    value,
+                    prev: None,
+                    next: None,
+                });
+
+                let node = vacant.insert(node);
+
+                if let Some(order) = &mut self.order {
+                    node.prev = Some(order.last);
+                    unsafe { order.last.as_mut() }.next = Some(NonNull::from(node.as_ref()));
+                    order.last = NonNull::from(node.as_ref());
+                } else {
+                    self.order = Some(InsertionOrder {
+                        first: NonNull::from(node.as_ref()),
+                        last: NonNull::from(node.as_ref()),
+                    });
+                }
+
+                None
+            }
+        }
+    }
+
+    pub fn unset(&mut self, key: &K) -> Option<V>
+    where
+        K: Hash + Eq,
+    {
+        match self.nodes.remove(key) {
+            Some(node) => {
+                match (node.prev, node.next) {
+                    (Some(mut prev), Some(mut next)) => {
+                        unsafe { prev.as_mut() }.next = Some(next);
+                        unsafe { next.as_mut() }.prev = Some(prev);
+                    }
+                    (Some(mut prev), None) => {
+                        unsafe { prev.as_mut() }.next = None;
+                        self.order.as_mut().unwrap().last = prev;
+                    }
+                    (None, Some(mut next)) => {
+                        unsafe { next.as_mut() }.prev = None;
+                        self.order.as_mut().unwrap().first = next;
+                    }
+                    (None, None) => self.order = None,
+                }
+                Some(node.value)
+            }
+            None => None,
+        }
+    }
+
+    pub fn keys(&self) -> Keys<K, V> {
+        Keys {
+            next_node: self.order.as_ref().map(|order| order.first),
+            phantom: PhantomData,
+        }
+    }
+}
+impl<K, V> Default for InsertionOrderHashMap<K, V> {
+    fn default() -> Self {
+        Self {
+            nodes: Default::default(),
+            order: Default::default(),
+        }
+    }
+}
+
+struct Node<K, V> {
+    key: NonNull<K>,
+    value: V,
+    prev: Option<NonNull<Node<K, V>>>,
+    next: Option<NonNull<Node<K, V>>>,
+}
+
+struct InsertionOrder<K, V> {
+    first: NonNull<Node<K, V>>,
+    last: NonNull<Node<K, V>>,
+}
+
+pub struct Keys<'a, K: 'a, V> {
+    next_node: Option<NonNull<Node<K, V>>>,
+    phantom: PhantomData<&'a K>,
+}
+impl<'a, K, V> Iterator for Keys<'a, K, V> {
+    type Item = &'a K;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match &self.next_node {
+            Some(node) => {
+                let key = unsafe { node.as_ref().key.as_ref() };
+                self.next_node = unsafe { node.as_ref() }.next;
+                Some(key)
+            }
+            None => None,
+        }
+    }
+}
---
+++ insertion_order_hash_map/src/tests.rs
@@ -0,0 +1,212 @@
+use std::ptr;
+
+use super::*;
+
+#[test]
+fn test_empty() {
+    let iohm: InsertionOrderHashMap<String, i32> = InsertionOrderHashMap::new();
+
+    assert!(iohm.nodes.is_empty());
+    assert!(iohm.order.is_none());
+}
+
+#[test]
+fn test_get_existing_key() {
+    let mut iohm = InsertionOrderHashMap::new();
+    iohm.set("A", 1);
+    let iohm = as_immutable(iohm);
+
+    let result = iohm.get(&"A");
+
+    assert_eq!(result, Some(&1));
+}
+
+#[test]
+fn test_get_non_existing_key() {
+    let mut iohm = InsertionOrderHashMap::new();
+    iohm.set("A", 1);
+    let iohm = as_immutable(iohm);
+
+    let result = iohm.get(&"B");
+
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_set_on_empty() {
+    let mut iohm = InsertionOrderHashMap::new();
+
+    let result = iohm.set("A", 1);
+
+    let iohm = as_immutable(iohm);
+    assert!(result.is_none());
+    assert_eq!(iohm.nodes.len(), 1);
+    let node: &Node<_, _> = &iohm.nodes[&"A"];
+    assert_eq!(unsafe { node.key.as_ref() }, &"A");
+    assert_eq!(node.value, 1);
+    assert_first_node(&iohm, node);
+    assert_last_node(&iohm, node);
+}
+
+#[test]
+fn test_set_on_non_empty() {
+    let mut iohm = InsertionOrderHashMap::new();
+    iohm.set("A", 1);
+
+    let result = iohm.set("B", 2);
+
+    let iohm = as_immutable(iohm);
+    assert!(result.is_none());
+    assert_eq!(iohm.nodes.len(), 2);
+    let node_a: &Node<_, _> = &iohm.nodes[&"A"];
+    let node_b: &Node<_, _> = &iohm.nodes[&"B"];
+    assert_eq!(unsafe { node_b.key.as_ref() }, &"B");
+    assert_eq!(node_b.value, 2);
+    assert_linked_nodes(node_a, node_b);
+    assert_last_node(&iohm, node_b);
+}
+
+#[test]
+fn test_set_existing_key() {
+    let mut iohm = InsertionOrderHashMap::new();
+    iohm.set("A", 1);
+
+    let result = iohm.set("A", 2);
+
+    let iohm = as_immutable(iohm);
+    assert_eq!(result, Some(1));
+    assert_eq!(iohm.nodes.len(), 1);
+    let node: &Node<_, _> = &iohm.nodes[&"A"];
+    assert_eq!(unsafe { node.key.as_ref() }, &"A");
+    assert_eq!(node.value, 2);
+    assert_first_node(&iohm, node);
+    assert_last_node(&iohm, node);
+}
+
+#[test]
+fn test_unset_first() {
+    let mut iohm = InsertionOrderHashMap::new();
+    iohm.set("A", 1);
+    iohm.set("B", 2);
+    iohm.set("C", 3);
+
+    let result = iohm.unset(&"A");
+
+    let iohm = as_immutable(iohm);
+    assert_eq!(result, Some(1));
+    assert_eq!(iohm.nodes.len(), 2);
+    let node: &Node<_, _> = &iohm.nodes[&"B"];
+    assert_first_node(&iohm, node);
+}
+
+#[test]
+fn test_unset_last() {
+    let mut iohm = InsertionOrderHashMap::new();
+    iohm.set("A", 1);
+    iohm.set("B", 2);
+    iohm.set("C", 3);
+
+    let result = iohm.unset(&"C");
+
+    let iohm = as_immutable(iohm);
+    assert_eq!(result, Some(3));
+    assert_eq!(iohm.nodes.len(), 2);
+    let node: &Node<_, _> = &iohm.nodes[&"B"];
+    assert_last_node(&iohm, node);
+}
+
+#[test]
+fn test_unset_in_the_middle() {
+    let mut iohm = InsertionOrderHashMap::new();
+    iohm.set("A", 1);
+    iohm.set("B", 2);
+    iohm.set("C", 3);
+
+    let result = iohm.unset(&"B");
+
+    let iohm = as_immutable(iohm);
+    assert_eq!(result, Some(2));
+    assert_eq!(iohm.nodes.len(), 2);
+    let node_a: &Node<_, _> = &iohm.nodes[&"A"];
+    let node_c: &Node<_, _> = &iohm.nodes[&"C"];
+    assert_linked_nodes(node_a, node_c);
+}
+
+#[test]
+fn test_unset_single_item() {
+    let mut iohm = InsertionOrderHashMap::new();
+    iohm.set("A", 1);
+
+    let result = iohm.unset(&"A");
+
+    let iohm = as_immutable(iohm);
+    assert_eq!(result, Some(1));
+    assert!(iohm.nodes.is_empty());
+    assert!(iohm.order.is_none());
+}
+
+#[test]
+fn test_unset_non_existing_key() {
+    let mut iohm = InsertionOrderHashMap::new();
+    iohm.set("A", 1);
+
+    let result = iohm.unset(&"B");
+
+    let iohm = as_immutable(iohm);
+    assert!(result.is_none());
+    assert_eq!(iohm.nodes.len(), 1);
+}
+
+#[test]
+fn test_keys_on_empty() {
+    let iohm: InsertionOrderHashMap<String, i32> = InsertionOrderHashMap::new();
+
+    let keys = iohm.keys();
+
+    let keys_vec: Vec<_> = keys.collect();
+    assert!(keys_vec.is_empty());
+}
+
+#[test]
+fn test_keys_on_non_empty() {
+    let mut iohm = InsertionOrderHashMap::new();
+    iohm.set("A", 1);
+    iohm.set("B", 2);
+    iohm.set("C", 3);
+    let iohm = as_immutable(iohm);
+
+    let keys = iohm.keys();
+
+    let keys_vec: Vec<_> = keys.collect();
+    assert_eq!(keys_vec, vec![&"A", &"B", &"C"]);
+}
+
+fn assert_first_node<K, V>(iohm: &InsertionOrderHashMap<K, V>, node: &Node<K, V>) {
+    let order = iohm.order.as_ref().expect("order should not be None");
+    assert!(ptr::eq(order.first.as_ptr(), node));
+
+    assert!(node.prev.is_none());
+}
+
+fn assert_last_node<K, V>(iohm: &InsertionOrderHashMap<K, V>, node: &Node<K, V>) {
+    let order = iohm.order.as_ref().expect("order should not be None");
+    assert!(ptr::eq(order.last.as_ptr(), node));
+
+    assert!(node.next.is_none());
+}
+
+fn assert_linked_nodes<K, V>(node_before: &Node<K, V>, node_after: &Node<K, V>) {
+    let node_before_next = node_before
+        .next
+        .expect("node_before.next should not be None");
+    let node_before_next = unsafe { node_before_next.as_ref() };
+    assert!(ptr::eq(node_before_next, node_after));
+
+    let node_after_prev = node_after.prev.expect("node_after.prev should not be None");
+    let node_after_prev = unsafe { node_after_prev.as_ref() };
+    assert!(ptr::eq(node_after_prev, node_before));
+}
+
+fn as_immutable<T>(value: T) -> T {
+    value
+}
