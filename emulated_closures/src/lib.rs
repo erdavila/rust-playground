4cc34f5d98665beaabf314bcd509a3ff803a0d2f
Eduardo R. D'Avila <erdavila@gmail.com>
2024-02-23 12:21:16 -0300

    emulated_closures: Implement emulated_fn_mut

--- emulated_closures/src/lib.rs
+++ emulated_closures/src/lib.rs
@@ -4,6 +4,10 @@ pub trait EmulatedFnOnce<Args> {
     fn call_once(self, args: Args) -> Self::Output;
 }
 
+pub trait EmulatedFnMut<Args>: EmulatedFnOnce<Args> {
+    fn call_mut(&mut self, args: Args) -> Self::Output;
+}
+
 #[macro_export]
 macro_rules! emulated_fn_once {
     (
@@ -39,11 +43,58 @@ macro_rules! emulated_fn_once {
 }
 
 #[macro_export]
+macro_rules! emulated_fn_mut {
+    (
+        captures: {
+            $( [ $($capture:tt)* ] ),*
+            $(,)?
+        },
+        signature: ( $($arg:ty),* $(,)? ) => $out:ty,
+        $body:expr
+    ) => {
+        {
+            #[allow(non_camel_case_types)]
+            struct impl_EmulatedFnMut<'lt> {
+                captures: ( $( $crate::__emulated_closures__fn_mut_capture_type!( $($capture)* ) , )* ),
+                _phantom: std::marker::PhantomData<&'lt ()>,
+            }
+
+            impl<'lt> $crate::EmulatedFnMut<( $( $arg , )* )> for impl_EmulatedFnMut<'lt> {
+                fn call_mut(&mut self, args: ( $( $arg , )* )) -> Self::Output {
+                    let body: fn(&mut ( $( $crate::__emulated_closures__fn_mut_capture_type!( $($capture)* ) , )* ), ( $( $arg , )* )) -> $out = $body;
+                    body(&mut self.captures, args)
+                }
+            }
+
+            impl<'lt> $crate::EmulatedFnOnce<( $( $arg , )* )> for impl_EmulatedFnMut<'lt> {
+                type Output = $out;
+
+                fn call_once(mut self, args: ( $( $arg , )* )) -> Self::Output {
+                    self.call_mut(args)
+                }
+            }
+
+            impl_EmulatedFnMut {
+                captures: ( $( $crate::__emulated_closures__capture_expr!( $($capture)* ) , )* ),
+                _phantom: std::marker::PhantomData,
+            }
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! __emulated_closures__fn_once_capture_type {
     ($expr:expr => &mut $ty:ty) => { &'lt mut $ty };
     ($expr:expr => & $ty:ty) => { &'lt $ty };
     ($expr:expr => move $ty:ty) => { $ty };
     ($expr:expr => $ty:ty) => { $ty };
+}
+
+#[macro_export]
+macro_rules! __emulated_closures__fn_mut_capture_type {
+    ($expr:expr => &mut $ty:ty) => { &'lt mut $ty };
+    ($expr:expr => & $ty:ty) => { &'lt $ty };
+    ($expr:expr => move $ty:ty) => { $ty };
 }
 
 #[macro_export]
@@ -202,6 +253,103 @@ mod tests {
             };
 
             let result = closure.call_once((7,));
+
+            assert_eq!(result, 14);
+        }
+    }
+
+    mod emulated_fn_mut {
+        use crate::{tests::Object, EmulatedFnMut};
+
+        use super::Usage;
+
+        #[test]
+        fn test_access() {
+            let obj_ref_usage = Usage::new();
+            let obj_ref = &obj_ref_usage.new_object();
+
+            let mut closure = emulated_fn_mut! {
+                captures: { [obj_ref => &Object] },
+                signature: () => (),
+                |(obj_ref,), ()| {
+                    obj_ref.access();
+                }
+            };
+
+            assert!(!obj_ref_usage.accessed());
+            closure.call_mut(());
+            assert!(obj_ref_usage.accessed());
+        }
+
+        #[test]
+        fn test_mutate() {
+            let obj_mut_ref_usage = Usage::new();
+            let obj_mut_ref = &mut obj_mut_ref_usage.new_object();
+
+            let mut closure = emulated_fn_mut! {
+                captures: { [obj_mut_ref => &mut Object] },
+                signature: () => (),
+                |(obj_ref,), ()| {
+                    obj_ref.mutate();
+                }
+            };
+
+            assert!(!obj_mut_ref_usage.mutated());
+            closure.call_mut(());
+            assert!(obj_mut_ref_usage.mutated());
+        }
+
+        #[test]
+        fn test_moved() {
+            let moved_usage = Usage::new();
+            let moved = moved_usage.new_object();
+
+            let mut closure = emulated_fn_mut! {
+                captures: { [moved => move Object] },
+                signature: () => (),
+                |(moved,), ()| {
+                    moved.access();
+                    moved.mutate();
+                }
+            };
+
+            assert!(!moved_usage.accessed());
+            assert!(!moved_usage.mutated());
+            closure.call_mut(());
+            assert!(moved_usage.accessed());
+            assert!(moved_usage.mutated());
+        }
+
+        #[test]
+        fn test_arguments() {
+            let mut x = 1;
+
+            let mut closure = emulated_fn_mut! {
+                captures: {[ &mut x => &mut u32 ]},
+                signature: (u32, u32) => u32,
+                |(x,), (a, b)| {
+                    **x += a;
+                    b
+                }
+            };
+
+            let result = closure.call_mut((3, 5));
+
+            assert_eq!(x, 4);
+            assert_eq!(result, 5);
+        }
+
+        #[test]
+        fn test_no_captures() {
+            let mut closure = emulated_fn_mut! {
+                captures: {},
+                signature: (u32,) => u32,
+                |(), (n,)| {
+                    2 * n
+                }
+            };
+
+            let result = closure.call_mut((7,));
 
             assert_eq!(result, 14);
         }
