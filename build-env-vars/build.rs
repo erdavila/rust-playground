d1ef97541571a04540a092a5bd5df6ec1e173ee2
Eduardo R. D'Avila <erdavila@gmail.com>
2024-03-06 17:03:49 -0300

    build-env-vars: Create it

---
+++ build-env-vars/build.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut vars: Vec<_> = std::env::vars().collect();
+    vars.sort_by_key(|(name, _value)| name.clone());
+    for (name, value) in vars {
+        println!("cargo:warning={:?}: {:?}", name, value);
+    }
+}
