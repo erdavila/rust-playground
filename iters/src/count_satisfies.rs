d70c6761750973884122698e3202dfde51f0632f
Eduardo R. D'Avila <erdavila@gmail.com>
2024-10-05 00:54:22 -0300

    iters/count_satisfies: Create macro

--- iters/src/count_satisfies.rs
+++ iters/src/count_satisfies.rs
@@ -40,6 +40,144 @@ impl N {
     }
 }
 
+#[macro_export]
+macro_rules! __count_satisfies_condition_comparison {
+    ($n:ident, ==, $compared:tt) => {
+        $n.eq($compared)
+    };
+    ($n:ident, !=, $compared:tt) => {
+        $n.ne($compared)
+    };
+    ($n:ident, <, $compared:tt) => {
+        $n.lt($compared)
+    };
+    ($n:ident, >, $compared:tt) => {
+        $n.gt($compared)
+    };
+    ($n:ident, <=, $compared:tt) => {
+        $n.le($compared)
+    };
+    ($n:ident, >=, $compared:tt) => {
+        $n.ge($compared)
+    };
+}
+
+#[macro_export]
+macro_rules! __count_satisfies_condition_parse_or_rest {
+    ($expr1:expr , $expr2:expr , && $n:ident $cmp:tt $compared:tt $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_or_rest!(
+            $expr1,
+            ( $expr2 ).and( $crate::__count_satisfies_condition_comparison!($n, $cmp, $compared) ),
+            $($rest)*
+        )
+    };
+    ($expr1:expr , $expr2:expr , && ( $($token:tt)+ ) $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_or_rest!(
+            $expr1,
+            ( $expr2 ).and( $crate::__count_satisfies_condition_parse!($($token)+) ),
+            $($rest)*
+        )
+    };
+    ($expr1:expr , $expr2:expr , && ! ( $($token:tt)+ ) $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_or_rest!(
+            $expr1,
+            ( $expr2 ).and( ( $crate::__count_satisfies_condition_parse!($($token)+) ).not() ),
+            $($rest)*
+        )
+    };
+    ($expr1:expr , $expr2:expr , $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_rest!(
+            ( $expr1 ).or( $expr2 ),
+            $($rest)*
+        )
+    };
+}
+
+#[macro_export]
+macro_rules! __count_satisfies_condition_parse_rest {
+    ($expr:expr , ) => {
+        $expr
+    };
+    ($expr:expr , && $n:ident $cmp:tt $compared:tt $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_rest!(
+            ( $expr ).and( $crate::__count_satisfies_condition_comparison!($n, $cmp, $compared) ),
+            $($rest)*
+        )
+    };
+    ($expr:expr , && ( $($token:tt)+ ) $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_rest!(
+            ( $expr ).and( $crate::__count_satisfies_condition_parse!($($token)+) ),
+            $($rest)*
+        )
+    };
+    ($expr:expr , && ! ( $($token:tt)+ ) $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_rest!(
+            ( $expr ).and(( $crate::__count_satisfies_condition_parse!($($token)+) ).not()),
+            $($rest)*
+        )
+    };
+    ($expr:expr , || $n:ident $cmp:tt $compared:tt $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_or_rest!(
+            $expr,
+            $crate::__count_satisfies_condition_comparison!($n, $cmp, $compared),
+            $($rest)*
+        )
+    };
+    ($expr:expr , || ( $($token:tt)+ ) $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_or_rest!(
+            $expr,
+            ( $crate::__count_satisfies_condition_parse!($($token)+) ),
+            $($rest)*
+        )
+    };
+    ($expr:expr , || ! ( $($token:tt)+ ) $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_or_rest!(
+            $expr,
+            ( $crate::__count_satisfies_condition_parse!($($token)+) ).not(),
+            $($rest)*
+        )
+    };
+}
+
+#[macro_export]
+macro_rules! __count_satisfies_condition_parse {
+    ($n:ident $cmp:tt $compared:tt $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_rest!(
+            $crate::__count_satisfies_condition_comparison!($n, $cmp, $compared),
+            $($rest)*
+        )
+    };
+    (( $($token:tt)+ ) $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_rest!(
+            $crate::__count_satisfies_condition_parse!($($token)+),
+            $($rest)*
+        )
+    };
+    (! ( $($token:tt)+ ) $($rest:tt)*) => {
+        $crate::__count_satisfies_condition_parse_rest!(
+            ( $crate::__count_satisfies_condition_parse!($($token)+) ).not(),
+            $($rest)*
+        )
+    };
+}
+
+#[macro_export]
+macro_rules! count_satisfies_condition {
+    (|$n:ident| { $($token:tt)+ }) => {
+        |$n| $crate::__count_satisfies_condition_parse!($($token)+)
+    };
+    (|$n:ident| $($token:tt)+) => {
+        |$n| $crate::__count_satisfies_condition_parse!($($token)+)
+    };
+}
+
+#[macro_export]
+macro_rules! count_satisfies {
+    ($iterator:expr, |$n:ident| $($token:tt)+) => {
+        $crate::CountSatisfies::count_satisfies($iterator, $crate::count_satisfies_condition!(|$n| $($token)+))
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use std::{cell::Cell, rc::Rc};
@@ -108,5 +246,75 @@ mod tests {
         assert_evaluation!((4 elems).count_satisfies(|n| n.le(4).and(n.ne(3)).and(n.gt(1))) should be true, 4);
         assert_evaluation!((5 elems).count_satisfies(|n| n.le(4).and(n.ne(3)).and(n.gt(1))) should be false, 5);
         assert_evaluation!((6 elems).count_satisfies(|n| n.le(4).and(n.ne(3)).and(n.gt(1))) should be false, 5);
+    }
+
+    #[test]
+    fn condition_macro() {
+        fn iter(len: usize) -> impl Iterator {
+            std::iter::repeat(()).take(len)
+        }
+
+        for len in 0..5 {
+            let expected = len < 2;
+            assert_eq!(
+                iter(len).count_satisfies(count_satisfies_condition!(|n| n < 2)),
+                expected,
+                "len={len}"
+            );
+            assert_eq!(
+                count_satisfies!(iter(len), |n| { n < 2 }),
+                expected,
+                "len={len}"
+            );
+            assert_eq!(
+                count_satisfies!(iter(len), |n| n < 2),
+                expected,
+                "len={len}"
+            );
+            assert_eq!(
+                count_satisfies!(iter(len), |n| (n < 2)),
+                expected,
+                "len={len}"
+            );
+            assert_eq!(
+                count_satisfies!(iter(len), |n| !(n < 2)),
+                !expected,
+                "len={len}"
+            );
+
+            let expected = len > 0 && len < 3 || len == 4;
+            assert_eq!(
+                count_satisfies!(iter(len), |n| n > 0 && n < 3 || n == 4),
+                expected,
+                "len={len}"
+            );
+            assert_eq!(
+                count_satisfies!(iter(len), |n| (n > 0) && (n < 3) || (n == 4)),
+                expected,
+                "len={len}"
+            );
+            assert_eq!(
+                count_satisfies!(iter(len), |n| !(n <= 0) && !(n >= 3) || !(n != 4)),
+                expected,
+                "len={len}"
+            );
+
+            let expected = len == 4 || len > 0 && len < 3;
+            assert_eq!(
+                count_satisfies!(iter(len), |n| n == 4 || n > 0 && n < 3),
+                expected,
+                "len={len}"
+            );
+            assert_eq!(
+                count_satisfies!(iter(len), |n| (n == 4) || (n > 0) && (n < 3)),
+                expected,
+                "len={len}"
+            );
+            assert_eq!(
+                count_satisfies!(iter(len), |n| !(n != 4) || !(n <= 0) && !(n >= 3)),
+                expected,
+                "len={len}"
+            );
+        }
     }
 }
