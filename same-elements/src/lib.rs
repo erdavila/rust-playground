aebf2305db0c0b4a528dfd6270e6afdfab0cbb49
Eduardo R. D'Avila <erdavila@gmail.com>
2024-12-18 12:00:40 -0300

    same_elements: Refactor common code

--- same-elements/src/lib.rs
+++ same-elements/src/lib.rs
@@ -3,60 +3,47 @@ use std::{
     hash::Hash,
 };
 
+macro_rules! same_elements {
+    ($a:ident, $b:ident, $map:expr, $namespace:ident) => {{
+        let a = $a;
+        let b = $b;
+        let mut map = $map;
+
+        for x in a {
+            map.entry(x)
+                .and_modify(|count| *count += 1)
+                .or_insert(0usize);
+        }
+
+        for y in b {
+            if let $namespace::Entry::Occupied(mut e) = map.entry(y) {
+                let count = e.get_mut();
+                if *count == 0 {
+                    e.remove();
+                } else {
+                    *count -= 1;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        map.is_empty()
+    }};
+}
+
 pub fn same_elements_hash<T: Eq + Hash>(
     a: impl IntoIterator<Item = T>,
     b: impl IntoIterator<Item = T>,
 ) -> bool {
-    let mut map = HashMap::new();
-
-    for x in a {
-        map.entry(x)
-            .and_modify(|count| *count += 1)
-            .or_insert(0usize);
-    }
-
-    for y in b {
-        if let hash_map::Entry::Occupied(mut e) = map.entry(y) {
-            let count = e.get_mut();
-            if *count == 0 {
-                e.remove();
-            } else {
-                *count -= 1;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    map.is_empty()
+    same_elements!(a, b, HashMap::new(), hash_map)
 }
 
 pub fn same_elements_ord<T: Ord>(
     a: impl IntoIterator<Item = T>,
     b: impl IntoIterator<Item = T>,
 ) -> bool {
-    let mut map = BTreeMap::new();
-
-    for x in a {
-        map.entry(x)
-            .and_modify(|count| *count += 1)
-            .or_insert(0usize);
-    }
-
-    for y in b {
-        if let btree_map::Entry::Occupied(mut e) = map.entry(y) {
-            let count = e.get_mut();
-            if *count == 0 {
-                e.remove();
-            } else {
-                *count -= 1;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    map.is_empty()
+    same_elements!(a, b, BTreeMap::new(), btree_map)
 }
 
 #[cfg(test)]
