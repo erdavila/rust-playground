063df857ae7ca15d3c69b1cecd3c31a7f01f4811
Eduardo R. D'Avila <erdavila@gmail.com>
2025-01-04 17:03:14 -0300

    min-max-binary-heap: Implement pop_{min,max}()

--- min-max-binary-heap/src/heap.rs
+++ min-max-binary-heap/src/heap.rs
@@ -36,6 +36,27 @@ where
         self.heap_up(self.entries.len() - 1);
     }
 
+    pub(crate) fn pop(&mut self) -> Option<EntryRef<T>> {
+        if self.entries.is_empty() {
+            None
+        } else {
+            let entry = self.remove(0);
+            Some(entry)
+        }
+    }
+
+    pub(crate) fn remove(&mut self, index: usize) -> EntryRef<T> {
+        let entry = self.entries.swap_remove(index);
+
+        if index < self.entries.len() {
+            O::set_heap_index(&mut self.entries[index].borrow_mut(), index);
+            self.heap_down(index);
+            self.heap_up(index);
+        }
+
+        entry
+    }
+
     fn heap_up(&mut self, mut index: usize) {
         while index > 0 {
             let parent_index = (index - 1) / 2;
@@ -43,7 +64,7 @@ where
             let mut entry = self.entries[index].borrow_mut();
             let mut parent_entry = self.entries[parent_index].borrow_mut();
 
-            if O::validate_order(&parent_entry.element, &entry.element) {
+            if O::comes_before(&parent_entry.element, &entry.element) {
                 break;
             }
 
@@ -55,6 +76,43 @@ where
             self.entries.swap(index, parent_index);
 
             index = parent_index;
+        }
+    }
+
+    fn heap_down(&mut self, mut index: usize) {
+        while index < self.entries.len() {
+            let left_index = 2 * index + 1;
+            let right_index = 2 * index + 2;
+
+            if left_index >= self.entries.len() {
+                break;
+            }
+
+            let mut child_index = left_index;
+            if right_index < self.entries.len()
+                && O::comes_before(
+                    &self.entries[right_index].borrow().element,
+                    &self.entries[left_index].borrow().element,
+                )
+            {
+                child_index = right_index;
+            }
+
+            let mut entry = self.entries[index].borrow_mut();
+            let mut child_entry = self.entries[child_index].borrow_mut();
+
+            if O::comes_before(&entry.element, &child_entry.element) {
+                break;
+            }
+
+            O::set_heap_index(&mut entry, child_index);
+            O::set_heap_index(&mut child_entry, index);
+
+            drop(entry);
+            drop(child_entry);
+            self.entries.swap(index, child_index);
+
+            index = child_index;
         }
     }
 }
@@ -70,38 +128,73 @@ where
     }
 }
 
-pub(crate) trait HeapOrder {
-    fn validate_order<T>(parent_value: &T, child_value: &T) -> bool
+pub(crate) trait HeapOrder: Sized {
+    type Other: HeapOrder;
+
+    fn comes_before<T>(a: &T, b: &T) -> bool
     where
         T: Ord;
 
+    fn get_heap_index<T>(entry: &Entry<T>) -> usize;
+
     fn set_heap_index<T>(entry: &mut Entry<T>, index: usize);
+
+    fn select_heaps_mut<'a, T>(
+        min_heap: &'a mut Heap<T, Min>,
+        max_heap: &'a mut Heap<T, Max>,
+    ) -> (&'a mut Heap<T, Self>, &'a mut Heap<T, Self::Other>);
 }
 
 pub(crate) enum Min {}
 impl HeapOrder for Min {
-    fn validate_order<T>(parent_value: &T, child_value: &T) -> bool
+    type Other = Max;
+
+    fn comes_before<T>(a: &T, b: &T) -> bool
     where
         T: Ord,
     {
-        parent_value <= child_value
+        a <= b
+    }
+
+    fn get_heap_index<T>(entry: &Entry<T>) -> usize {
+        entry.min_heap_index
     }
 
     fn set_heap_index<T>(entry: &mut Entry<T>, index: usize) {
         entry.min_heap_index = index;
     }
+
+    fn select_heaps_mut<'a, T>(
+        min_heap: &'a mut Heap<T, Min>,
+        max_heap: &'a mut Heap<T, Max>,
+    ) -> (&'a mut Heap<T, Self>, &'a mut Heap<T, Self::Other>) {
+        (min_heap, max_heap)
+    }
 }
 
 pub(crate) enum Max {}
 impl HeapOrder for Max {
-    fn validate_order<T>(parent_value: &T, child_value: &T) -> bool
+    type Other = Min;
+
+    fn comes_before<T>(a: &T, b: &T) -> bool
     where
         T: Ord,
     {
-        parent_value >= child_value
+        a >= b
+    }
+
+    fn get_heap_index<T>(entry: &Entry<T>) -> usize {
+        entry.max_heap_index
     }
 
     fn set_heap_index<T>(entry: &mut Entry<T>, index: usize) {
         entry.max_heap_index = index;
+    }
+
+    fn select_heaps_mut<'a, T>(
+        min_heap: &'a mut Heap<T, Min>,
+        max_heap: &'a mut Heap<T, Max>,
+    ) -> (&'a mut Heap<T, Max>, &'a mut Heap<T, Min>) {
+        (max_heap, min_heap)
     }
 }
--- min-max-binary-heap/src/lib.rs
+++ min-max-binary-heap/src/lib.rs
@@ -2,7 +2,7 @@ mod heap;
 
 use std::{cell::RefCell, collections::TryReserveError, fmt::Debug, marker::PhantomData, rc::Rc};
 
-use heap::{Entry, Heap, Max, Min};
+use heap::{Entry, Heap, HeapOrder, Max, Min};
 
 pub struct MinMaxBinaryHeap<T> {
     min_heap: Heap<T, Min>,
@@ -90,11 +90,30 @@ where
     }
 
     pub fn pop_min(&mut self) -> Option<T> {
-        todo!()
+        self.pop::<Min>()
     }
 
     pub fn pop_max(&mut self) -> Option<T> {
-        todo!()
+        self.pop::<Max>()
+    }
+
+    fn pop<O>(&mut self) -> Option<T>
+    where
+        O: HeapOrder,
+    {
+        let (heap, other_heap) = O::select_heaps_mut(&mut self.min_heap, &mut self.max_heap);
+        if let Some(entry) = heap.pop() {
+            let other_heap_index = O::Other::get_heap_index(&entry.borrow());
+            other_heap.remove(other_heap_index);
+
+            let Some(entry) = Rc::into_inner(entry) else {
+                unreachable!()
+            };
+            let entry = RefCell::into_inner(entry);
+            Some(entry.element)
+        } else {
+            None
+        }
     }
 
     pub fn push(&mut self, element: T) {
@@ -266,26 +285,36 @@ mod tests {
 
     macro_rules! assert_state {
         ($heap:ident) => {{
-            assert_eq!($heap.min_heap.len(), $heap.max_heap.len());
+            assert_eq!(
+                $heap.min_heap.len(),
+                $heap.max_heap.len(),
+                "heap sizes do not match"
+            );
 
             for i in 0..$heap.min_heap.len() {
                 let min_heap_entry = $heap.min_heap[i].borrow();
                 let max_heap_entry = $heap.max_heap[i].borrow();
 
-                assert_eq!(min_heap_entry.min_heap_index, i);
-                assert_eq!(max_heap_entry.max_heap_index, i);
+                assert_eq!(min_heap_entry.min_heap_index, i, "wrong min_heap_index");
+                assert_eq!(max_heap_entry.max_heap_index, i, "wrong max_heap_index");
 
                 if i > 0 {
                     let parent = (i - 1) / 2;
-                    assert!($heap.min_heap[parent].borrow().element <= min_heap_entry.element);
-                    assert!($heap.max_heap[parent].borrow().element >= max_heap_entry.element);
+                    assert!(
+                        $heap.min_heap[parent].borrow().element <= min_heap_entry.element,
+                        "min_heap invariant violated at {i}"
+                    );
+                    assert!(
+                        $heap.max_heap[parent].borrow().element >= max_heap_entry.element,
+                        "max_heap invariant violated at {i}"
+                    );
                 }
 
                 let entry_rc = &$heap.min_heap[i];
-                assert!(Rc::ptr_eq(
-                    entry_rc,
-                    &$heap.max_heap[entry_rc.borrow().max_heap_index]
-                ));
+                assert!(
+                    Rc::ptr_eq(entry_rc, &$heap.max_heap[entry_rc.borrow().max_heap_index]),
+                    "min_heap and max_heap entries do not point to the same object at {i}"
+                );
             }
         }};
     }
@@ -307,6 +336,66 @@ mod tests {
         assert_state!(heap);
 
         heap.push(5);
+        assert_state!(heap);
+    }
+
+    #[test]
+    fn pop() {
+        let mut heap = MinMaxBinaryHeap::new();
+        heap.push(2);
+        heap.push(1);
+        heap.push(8);
+        heap.push(4);
+        // Current elements: [1, 2, 4, 8]
+
+        assert_eq!(heap.pop_min(), Some(1));
+        assert_state!(heap);
+        // Current elements: [2, 4, 8]
+
+        heap.push(3);
+        // Current elements: [2, 3, 4, 8]
+
+        assert_eq!(heap.pop_max(), Some(8));
+        assert_state!(heap);
+        // Current elements: [2, 3, 4]
+
+        heap.push(7);
+        // Current elements: [2, 3, 4, 7]
+
+        assert_eq!(heap.pop_min(), Some(2));
+        assert_state!(heap);
+        // Current elements: [3, 4, 7]
+
+        heap.push(5);
+        // Current elements: [3, 4, 5, 7]
+
+        assert_eq!(heap.pop_max(), Some(7));
+        assert_state!(heap);
+        // Current elements: [3, 4, 5]
+
+        heap.push(6);
+        // Current elements: [3, 4, 5, 6]
+
+        assert_eq!(heap.pop_min(), Some(3));
+        assert_state!(heap);
+        // Current elements: [4, 5, 6]
+
+        assert_eq!(heap.pop_max(), Some(6));
+        assert_state!(heap);
+        // Current elements: [4, 5]
+
+        assert_eq!(heap.pop_min(), Some(4));
+        assert_state!(heap);
+        // Current elements: [5]
+
+        assert_eq!(heap.pop_max(), Some(5));
+        assert_state!(heap);
+        // Current elements: []
+
+        assert_eq!(heap.pop_min(), None);
+        assert_state!(heap);
+
+        assert_eq!(heap.pop_max(), None);
         assert_state!(heap);
     }
 }
