5dcf9407b0e52949ab07dac4901cd1863f136e82
Eduardo R. D'Avila <erdavila@gmail.com>
2025-01-20 11:26:26 -0300

    hlist: Implement const len() and is_empty() methods and hnil() and hcons() functions

--- hlist/src/lib.rs
+++ hlist/src/lib.rs
@@ -4,23 +4,39 @@
 
 /// The abstract representation of heterogeneous lists.
 pub trait HList {
+    /// The length of the heterogeneous list.
+    const LENGTH: usize;
+
     /// Provides the length of the heterogeneous list.
     fn len(&self) -> usize;
 
     /// Checks if the heterogeneous list is [`HNil`]
-    fn is_empty(&self) -> bool;
+    fn is_empty(&self) -> bool {
+        Self::LENGTH == 0
+    }
 }
 
 /// The empty [`HList`].
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct HNil;
-impl HList for HNil {
-    fn len(&self) -> usize {
-        0
+impl HNil {
+    /// Provides the length of the heterogeneous list in a `const` context.
+    #[must_use]
+    pub const fn len(&self) -> usize {
+        Self::LENGTH
     }
 
-    fn is_empty(&self) -> bool {
-        true
+    /// Checks if the heterogeneous list is [`HNil`] in a `const` context.
+    #[must_use]
+    pub const fn is_empty(&self) -> bool {
+        Self::LENGTH == 0
+    }
+}
+impl HList for HNil {
+    const LENGTH: usize = 0;
+
+    fn len(&self) -> usize {
+        Self::LENGTH
     }
 }
 
@@ -44,18 +60,42 @@ where
     pub const fn new(head: H, tail: T) -> Self {
         HCons { head, tail }
     }
+
+    /// Provides the length of the heterogeneous list in a `const` context.
+    pub const fn len(&self) -> usize {
+        Self::LENGTH
+    }
+
+    /// Checks if the heterogeneous list is [`HNil`] in a `const` context.
+    pub const fn is_empty(&self) -> bool {
+        Self::LENGTH == 0
+    }
 }
 impl<H, T> HList for HCons<H, T>
 where
     T: HList,
 {
-    fn len(&self) -> usize {
-        1 + self.tail.len()
-    }
+    const LENGTH: usize = 1 + T::LENGTH;
 
-    fn is_empty(&self) -> bool {
-        false
+    fn len(&self) -> usize {
+        Self::LENGTH
     }
+}
+
+/// Creates an empty [`HList`].
+#[must_use]
+pub const fn hnil() -> HNil {
+    HNil
+}
+
+/// Creates a non-empty [`HList`].
+///
+/// It is an alias for [`HCons::new`].
+pub const fn hcons<H, T>(head: H, tail: T) -> HCons<H, T>
+where
+    T: HList,
+{
+    HCons::new(head, tail)
 }
 
 #[cfg(test)]
@@ -103,20 +143,43 @@ mod tests {
     }
 
     #[test]
-    fn len_and_is_empty() {
-        let hlist = HNil;
+    fn const_len_and_is_empty() {
+        let hlist = hnil();
         assert_eq!(hlist.len(), 0);
         assert!(hlist.is_empty());
 
-        let hlist = HCons::new(123, HNil);
+        let hlist = hcons(123, HNil);
         assert_eq!(hlist.len(), 1);
         assert!(!hlist.is_empty());
 
-        let hlist = HCons::new(123, HCons::new("abc", HNil));
+        let hlist = hcons(123, hcons("abc", HNil));
         assert_eq!(hlist.len(), 2);
         assert!(!hlist.is_empty());
 
-        let hlist = HCons::new(123, HCons::new("abc", HCons::new(true, HNil)));
+        let hlist = hcons(123, hcons("abc", hcons(true, HNil)));
+        assert_eq!(hlist.len(), 3);
+        assert!(!hlist.is_empty());
+    }
+
+    #[test]
+    fn trait_len_and_is_empty() {
+        fn create(hlist: impl HList) -> impl HList {
+            hlist
+        }
+
+        let hlist = create(HNil);
+        assert_eq!(hlist.len(), 0);
+        assert!(hlist.is_empty());
+
+        let hlist = create(HCons::new(123, HNil));
+        assert_eq!(hlist.len(), 1);
+        assert!(!hlist.is_empty());
+
+        let hlist = create(HCons::new(123, HCons::new("abc", HNil)));
+        assert_eq!(hlist.len(), 2);
+        assert!(!hlist.is_empty());
+
+        let hlist = create(HCons::new(123, HCons::new("abc", HCons::new(true, HNil))));
         assert_eq!(hlist.len(), 3);
         assert!(!hlist.is_empty());
     }
