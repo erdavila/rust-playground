c89a7bd15a2c6c8f36ae161f7912bbaf7a68bd05
Eduardo R. D'Avila <erdavila@gmail.com>
2026-01-12 12:15:54 -0300

    build-env-vars: Fix pedantic lint warnings and migrate to Rust edition 2024

--- build-env-vars/build.rs
+++ build-env-vars/build.rs
@@ -2,6 +2,6 @@ fn main() {
     let mut vars: Vec<_> = std::env::vars().collect();
     vars.sort_by_key(|(name, _value)| name.clone());
     for (name, value) in vars {
-        println!("cargo:warning={:?}: {:?}", name, value);
+        println!("cargo:warning={name:?}: {value:?}");
     }
 }
